646c1a164fe8b92f7ce4f8f64407586c2cdef15d
yessad <39194153+moh3424@users.noreply.github.com>
2018-12-12 12:25:49 +0100

    ajout de la base

---
+++ src/Controller/portfoliosymfony.sql
@@ -0,0 +1,458 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  mer. 12 déc. 2018 à 12:22
+-- Version du serveur :  10.1.34-MariaDB
+-- Version de PHP :  7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `portfoliosymfony`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `back`
+--
+
+CREATE TABLE `back` (
+  `id` int(11) NOT NULL,
+  `name` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `niveau` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `back`
+--
+
+INSERT INTO `back` (`id`, `name`, `photo`, `niveau`) VALUES
+(1, 'PHP7', 'php1.png', 70),
+(3, 'MySql', 'mysql1.png', 60);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cms`
+--
+
+CREATE TABLE `cms` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `niveau` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `cms`
+--
+
+INSERT INTO `cms` (`id`, `name`, `photo`, `niveau`) VALUES
+(2, 'Wordpress', 'glass-wordpress-logo1.png', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `experience`
+--
+
+CREATE TABLE `experience` (
+  `id` int(11) NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `annee_debut` datetime NOT NULL,
+  `date_fin` datetime NOT NULL,
+  `entreprise` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `site_web` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dep_service` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `experience`
+--
+
+INSERT INTO `experience` (`id`, `type`, `annee_debut`, `date_fin`, `entreprise`, `site_web`, `dep_service`) VALUES
+(1, 'Technicien Supérieur en Informatique', '2009-12-10 00:00:00', '2010-12-10 00:00:00', 'Ecole Mimoune (ALGERIE)', '', 0),
+(2, 'Intégrateur/ Développeur Web', '2018-12-10 00:00:00', '2019-12-10 00:00:00', 'LepoleS WebForce3', 'http://www.wf3.fr/', 92);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `formation`
+--
+
+CREATE TABLE `formation` (
+  `id` int(11) NOT NULL,
+  `diplome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `annee_scolaire` datetime NOT NULL,
+  `institut` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mention` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `formation`
+--
+
+INSERT INTO `formation` (`id`, `diplome`, `annee_scolaire`, `institut`, `mention`) VALUES
+(1, 'Technicien Supérieur', '2008-12-10 00:00:00', 'INTHT (ALGERIE)', 'Bien'),
+(2, 'Intégrateur / Développeur Web', '2019-12-10 00:00:00', 'LepoleS WebForce3', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `framework`
+--
+
+CREATE TABLE `framework` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `niveau` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `framework`
+--
+
+INSERT INTO `framework` (`id`, `name`, `photo`, `niveau`) VALUES
+(2, 'Symfony 4', 'symf1.jpg', 60),
+(3, 'Ionic', 'ionic-square1.jpg', 35),
+(4, 'Angular', 'angular1.png', 65);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `front`
+--
+
+CREATE TABLE `front` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `niveau` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `front`
+--
+
+INSERT INTO `front` (`id`, `name`, `photo`, `niveau`) VALUES
+(1, 'HTML 5', 'html1.png', 98),
+(2, 'CSS3', 'css31.png', 80),
+(19, 'JavaScript', 'js1.jpg', 93);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `loisir`
+--
+
+CREATE TABLE `loisir` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `icon` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `loisir`
+--
+
+INSERT INTO `loisir` (`id`, `name`, `photo`, `icon`) VALUES
+(1, 'Plongée sous marine', 'spot-plongee-sous-marine.jpg', '<i class=\"fas fa-swimmer\"></i>'),
+(2, 'Voyage', 'voyage.jpg', '<i class=\"fas fa-fighter-jet\"></i>'),
+(3, 'Cuisine', 'images.jpg', '<i class=\"fas fa-mortar-pestle\"></i> 	');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
+  `pseudo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `message` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `add_create` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `migration_versions`
+--
+
+CREATE TABLE `migration_versions` (
+  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `migration_versions`
+--
+
+INSERT INTO `migration_versions` (`version`) VALUES
+('20181204110335'),
+('20181205085335'),
+('20181205094841'),
+('20181205100743'),
+('20181205101857'),
+('20181211104714'),
+('20181211110256');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `profil`
+--
+
+CREATE TABLE `profil` (
+  `id` int(11) NOT NULL,
+  `presentation` longtext COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `profil`
+--
+
+INSERT INTO `profil` (`id`, `presentation`) VALUES
+(1, 'Passionné de numérique, je me suis formé en autodidacte sur les langages web depuis plusieurs années.<br>\r\n \r\nActuellement en formation de développeur web, je suis à la recherche d\'un stage de deux mois (Janvier/février 2019).');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reseau`
+--
+
+CREATE TABLE `reseau` (
+  `id` int(11) NOT NULL,
+  `url_reseau` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `reseau`
+--
+
+INSERT INTO `reseau` (`id`, `url_reseau`) VALUES
+(1, '<a href=\"https://www.linkedin.com/in/mohamed-yessad-4a51b016a/\" target=\"_blank\"><i class=\"fab fa-linkedin fa-2x fa-fw\"></i></a>');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `specialite`
+--
+
+CREATE TABLE `specialite` (
+  `id` int(11) NOT NULL,
+  `intitule_spec` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `specialite`
+--
+
+INSERT INTO `specialite` (`id`, `intitule_spec`) VALUES
+(1, 'DÉVELOPPEUR/INTÉGRATEUR WEB 	');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `civilite` varchar(4) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `photo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` longtext COLLATE utf8mb4_unicode_ci,
+  `ville` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `code_postal` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `firstname`, `lastname`, `civilite`, `photo`, `phone`, `address`, `ville`, `code_postal`) VALUES
+(1, 'mohamed', 'mohamed.yessad@lepoles.com', '$2y$13$6avdYUi9CJ5QKqwQKrwGAeYPhmGsJCpxfPRWbCH3OJb3rtDhb6n5K', 'Yessad', 'Mohamed', 'M.', 'moi.jpg', '06 67 80 55 98', '19 Rue laperouse', 'Pantin', 93);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `back`
+--
+ALTER TABLE `back`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `cms`
+--
+ALTER TABLE `cms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `experience`
+--
+ALTER TABLE `experience`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `formation`
+--
+ALTER TABLE `formation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `framework`
+--
+ALTER TABLE `framework`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `front`
+--
+ALTER TABLE `front`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `loisir`
+--
+ALTER TABLE `loisir`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `migration_versions`
+--
+ALTER TABLE `migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Index pour la table `profil`
+--
+ALTER TABLE `profil`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `reseau`
+--
+ALTER TABLE `reseau`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `specialite`
+--
+ALTER TABLE `specialite`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `back`
+--
+ALTER TABLE `back`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `cms`
+--
+ALTER TABLE `cms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `experience`
+--
+ALTER TABLE `experience`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `formation`
+--
+ALTER TABLE `formation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `framework`
+--
+ALTER TABLE `framework`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `front`
+--
+ALTER TABLE `front`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT pour la table `loisir`
+--
+ALTER TABLE `loisir`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `profil`
+--
+ALTER TABLE `profil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `reseau`
+--
+ALTER TABLE `reseau`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `specialite`
+--
+ALTER TABLE `specialite`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
